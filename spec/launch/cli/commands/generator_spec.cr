ca2510fb7f00f8b908020b155461a5490e9ef6e0
= <awcrotwell@gmail.com>
2020-11-11 14:58:13 -0500

    Fix model generation spec

--- spec/launch/cli/commands/generator_spec.cr
+++ spec/launch/cli/commands/generator_spec.cr
@@ -7,6 +7,14 @@ include CLIFixtures
 
 module Launch::CLI
   describe "launch generate" do
+    before_each do
+      scaffold_app(TESTING_APP)
+    end
+
+    after_each do
+      cleanup
+    end
+
     ENV["LAUNCH_ENV"] = "test"
     camel_case = "PostComment"
     snake_case = "post_comment"
@@ -16,8 +24,6 @@ module Launch::CLI
     spec_definition_prefix = "describe #{camel_case}"
 
     it "generates controller with correct verbs and actions" do
-      scaffold_app(TESTING_APP)
-      # options = %w(add:post list:get remove:delete)
       MainCommand.run %w(generate controller -y animal)
       route_file = File.read("./config/routes.cr")
 
@@ -41,32 +47,28 @@ module Launch::CLI
         File.delete(src_filepath)
         File.delete(spec_filepath)
       end
-      cleanup
     end
 
-    pending("generates model correctly")
-    # it "generates model correctly" do
-    #   scaffold_app(TESTING_APP)
-    #   MainCommand.run %w(
-    #     generate model -y Post title:string body:text published:bool likes:int user:references
-    #   )
+    # pending("generates model correctly")
+    it "generates model correctly" do
+      MainCommand.run %w(
+        generate model -y Post title:string body:text published:bool likes:int user:references
+      )
 
-    #   # "creates Post migration file"
-    #   generated_migration = Dir["./db/migrations/*_post.sql"].first
-    #   File.read(generated_migration).should eq expected_post_model_migration
+      # "creates Post migration file"
+      generated_migration = Dir["./db/migrations/*_create_post.cr"].first
+      File.read(generated_migration).should eq expected_post_model_migration
 
-    #   # "generates Post model spec file"
-    #   generated_spec = "./spec/models/post_spec.cr"
-    #   File.read(generated_spec).should eq expected_post_model_spec
+      # "generates Post model spec file"
+      generated_spec = "./spec/models/post_spec.cr"
+      File.read(generated_spec).should eq expected_post_model_spec
 
-    #   # "generates Post model"
-    #   generated_model = "src/models/post.cr"
-    #   File.read(generated_model).should eq expected_post_model
-    #   cleanup
-    # end
+      # "generates Post model"
+      generated_model = "src/models/post.cr"
+      File.read(generated_model).should eq expected_post_model
+    end
 
     it "generates scaffold correctly" do
-      scaffold_app(TESTING_APP)
       # "follows naming conventions for all files and class names"
       [camel_case, snake_case].each do |arg|
         MainCommand.run ["generate", "scaffold", "-y", arg, "name:string"]
@@ -92,11 +94,9 @@ module Launch::CLI
         File.read("./config/routes.cr").should contain "#{camel_case}Controller"
         File.read("./config/routes.cr").should_not contain "#{incorrect_case}Controller"
       end
-      cleanup
     end
 
     it "generates migrations correctly" do
-      scaffold_app(TESTING_APP)
       # "follows naming conventions for all files"
       [camel_case, snake_case].each do |arg|
         MainCommand.run ["generate", "migration", "-y", arg]
@@ -105,11 +105,9 @@ module Launch::CLI
         migration_filename.should contain snake_case
         File.delete("./db/migrations/#{migration_filename}")
       end
-      cleanup
     end
 
     it "generates mailers correctly" do
-      scaffold_app(TESTING_APP)
       # "follows naming conventions for all files and class names"
       [camel_case, snake_case].each do |arg|
         MainCommand.run ["generate", "mailer", "-y", arg]
@@ -120,11 +118,9 @@ module Launch::CLI
         File.read(src_filepath).should contain class_definition_prefix
         File.delete(src_filepath)
       end
-      cleanup
     end
 
     it "generatres socket correctly" do
-      scaffold_app(TESTING_APP)
       struct_definition_prefix = "struct #{camel_case}"
 
       # "follows naming conventions for all files and class names"
@@ -137,11 +133,9 @@ module Launch::CLI
         File.read(src_filepath).should contain struct_definition_prefix
         File.delete(src_filepath)
       end
-      cleanup
     end
 
     it "generates channels correctly" do
-      scaffold_app(TESTING_APP)
       # "follows naming conventions for all files and class names"
       [camel_case, snake_case].each do |arg|
         MainCommand.run ["generate", "channel", "-y", arg]
@@ -152,7 +146,6 @@ module Launch::CLI
         File.read(src_filepath).should contain class_definition_prefix
         File.delete(src_filepath)
       end
-      cleanup
     end
   end
 end
